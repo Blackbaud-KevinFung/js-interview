1bd0b25b2033fbb676de6ad970240534577d41cd
Blackbaud-KevinFung <kevin.fung@blackbaud.com>
2019-05-11 14:27:24 -0500

    removed heroes component import from app modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UsersComponent } from './users/users.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
