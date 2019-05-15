39c0c6b75a886cb6fbd544a32aaa053178c14233
Blackbaud-KevinFung <kevin.fung@blackbaud.com>
2019-05-14 22:36:06 -0500

    GREEN

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
 import { AddUpdateUser, AddUserResponse, ReqresService } from '../reqres.service';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-users',
@@ -8,9 +9,14 @@ import { AddUpdateUser, AddUserResponse, ReqresService } from '../reqres.service
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
+  public static readonly rowHeightXSmall = '2:1';
+  public static readonly rowHeightSmall = '3:1';
+  public static readonly rowHeightMedium = '5:1';
   public users: User[];
+  public columns = 2;
+  public rowHeight = UsersComponent.rowHeightMedium;
 
-  constructor(private reqresService: ReqresService) {
+  constructor(private reqresService: ReqresService, private breakpointObserver: BreakpointObserver) {
   }
 
   ngOnInit() {
@@ -18,6 +24,24 @@ export class UsersComponent implements OnInit {
         (users) => {
           this.users = users;
         });
+    this.breakpointObserver.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium
+    ]).subscribe(result => {
+      if (result.breakpoints[Breakpoints.XSmall]) {
+          this.columns = 1;
+          this.rowHeight = UsersComponent.rowHeightXSmall;
+      }
+      if (result.breakpoints[Breakpoints.Small]) {
+          this.columns = 2;
+          this.rowHeight = UsersComponent.rowHeightSmall;
+      }
+      if (result.breakpoints[Breakpoints.Medium]) {
+          this.columns = 2;
+          this.rowHeight = UsersComponent.rowHeightMedium;
+      }
+    });
   }
 
   public handleDelete(deleteUser: User) {
